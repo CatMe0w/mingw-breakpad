eb41c15166c0cdbf0f7e8e89efc8884f2c6eb224
GlassOfWhiskey <iacopo.c92@gmail.com>
2022-09-07 14:22:20 +0200

    Added crash context float state flag
    Instead of listing everywhere the set of architectures that do not
    require/support explicit float state in their crash context, a new
    GOOGLE_BREAKPAD_CRASH_CONTEXT_HAS_FLOAT_STATE preprocessor macro has
    been defined.
    
    Adding novel architectures will only require to manage the
    macro definition in a single place.
    
    Change-Id: I2732982f2cdfc9fcd2f71d6e5e122617faff9e82
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/3876345
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    
    # Conflicts:
    #	src/client/linux/handler/exception_handler.cc
    #	src/client/linux/handler/exception_handler.h

--- src/client/linux/handler/exception_handler.h
+++ src/client/linux/handler/exception_handler.h
@@ -201,10 +201,7 @@ class ExceptionHandler {
     siginfo_t siginfo;
     pid_t tid;  // the crashing thread.
     ucontext_t context;
-#if !defined(__ARM_EABI__) && !defined(__mips__)
-    // #ifdef this out because FP state is not part of user ABI for Linux ARM.
-    // In case of MIPS Linux FP state is already part of ucontext_t so
-    // 'float_state' is not required.
+#if GOOGLE_BREAKPAD_CRASH_CONTEXT_HAS_FLOAT_STATE
     fpstate_t float_state;
 #endif
   };
